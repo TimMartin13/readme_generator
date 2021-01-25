4968290014060553a095d6624657497474e5e3e5
Mandelbrot <timmartin13@gmail.com>
2021-01-25 17:54:45 -0600

    Added some questions that I might want to ask.

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = ["Project title?", "Project description?", "How do you install it?", "How do you use it?", "Did anyone else work on it?", "What kind of license would you like?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
